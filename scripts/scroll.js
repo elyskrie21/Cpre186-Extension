7258e27ad73dcf779450a60f981c7a0a5fa42822
Elyse Kriegel <esamkriegel@gmail.com>
2021-03-23 12:58:37 -0500

    changed the manifest

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,34 +1,34 @@
 function scrollToRecipe() {
-  let printElement;
-  let aElments = document.body.querySelectorAll("a");
+    let printElement;
+    let aElments = document.body.querySelectorAll("a");
 
-  for (const element of aElments) {
-    console.log("Here is an element: " + element);
-    text = element.textContent.toLowerCase();
-    if (text.includes("print") || text.includes("print recipe")) {
-      printElement = element;
-      break;
+    for (const element of aElments) {
+        console.log("Here is an element: " + element);
+        text = element.textContent.toLowerCase();
+        if (text.includes("print") || text.includes("print recipe")) {
+            printElement = element;
+            break;
+        }
     }
-  }
 
-  if (printElement == undefined) {
-    let buttons = document.body.querySelectorAll("button");
+    if (printElement == undefined) {
+        let buttons = document.body.querySelectorAll("button");
 
-    for (const element of buttons) {
-      text = element.textContent.toLowerCase();
-      if (text.includes("print") || text.includes("print recipe")) {
-        printElement = element;
-        break; 
-      }
+        for (const element of buttons) {
+            text = element.textContent.toLowerCase();
+            if (text.includes("print") || text.includes("print recipe")) {
+                printElement = element;
+                break;
+            }
+        }
     }
-  }
 
-  console.log(`This is the element chosen: ${printElement.textContent}`);
+    console.log(`This is the element chosen: ${printElement.textContent}`);
 
-  printElement.scrollIntoView(true, {
-    behavior: "smooth",
-    block: "end",
-  });
+    printElement.scrollIntoView(true, {
+        behavior: "smooth",
+        block: "end",
+    });
 }
 
 scrollToRecipe();
