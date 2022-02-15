bf47aae21757bcf6eeeb2299bec92c8386ec49ae
Elyse Kriegel <esamkriegel@gmail.com>
2022-02-15 03:10:45 -0600

    made priceCheck async

--- scripts/pricecheck.js
+++ scripts/pricecheck.js
@@ -8,11 +8,13 @@ Todo: Create function to send data to background.js
 */
 
 // *Testing with a static urls before dynamic 
-fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
-    .then(response => response.text())
-    .then(data => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(data, 'text/html')
+async function priceCheck()
+{
+    let response = await fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
+    let data = await response.text()
 
-        console.log(doc)
-    })
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(data, 'text/html')
+
+    console.log(doc)
+}
