209a93761f1c857fe91827e2637757116179d11c
Elyse Kriegel <esamkriegel@gmail.com>
2022-02-15 03:01:38 -0600

    started working on addProduct and priceCheck scripts

--- scripts/addproduct.js
+++ scripts/addproduct.js
@@ -5,3 +5,27 @@
 Todo: Create html parser to get product: name, price, url, and image 
 Todo: Create function to send data back to background.js
 */
+function sendData(url, name, price, images)
+{
+    // TODO
+}
+
+function addProduct() {
+    let url = window.location.href;
+    let productName = document.getElementById('productTitle').innerText; 
+
+    let productPrice = document.getElementsByClassName('a-price-whole')[0].innerText;
+    productPrice = productPrice.substring(0, productPrice.match('\n.').index)
+
+    let imgs = document.getElementById('main-image-container').getElementsByClassName('a-unordered-list')[0].getElementsByTagName('li')
+    let productImg = new Array()
+
+    for (img of imgs)
+    {
+        if(img.getElementsByTagName('img')[0] != null) {productImg.push(img.getElementsByTagName('img')[0].src)}
+    }
+    
+    sendData(url, productName, productPrice, productImg)
+}
+
+addProduct(); 
--- scripts/pricecheck.js
+++ scripts/pricecheck.js
@@ -6,3 +6,13 @@ Todo: Create function to get data from chrome.storage
 Todo: Create function to fetch Amazon html data
 Todo: Create function to send data to background.js
 */
+
+// *Testing with a static urls before dynamic 
+fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
+    .then(response => response.text())
+    .then(data => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(data, 'text/html')
+
+        console.log(doc)
+    })
