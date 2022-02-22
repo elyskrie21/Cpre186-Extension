a41465bb60739baa63998b655d2b8c240a63fc75
Elyse Kriegel <esamkriegel@gmail.com>
2022-02-22 12:15:22 -0600

    Worked on fetch function on background.js

--- popup.js
+++ popup.js
@@ -23,6 +23,7 @@ addProduct.addEventListener('click', async () => {
 // !This for testing only! will be controlled by background.js later
 let priceCheck = document.getElementById('check-price')
 priceCheck.addEventListener('click', async () => {
+    alert("this button was cliccked")
     // let response = await fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
     // let data = await response.text()
     // console.log(data)
@@ -32,19 +33,24 @@ priceCheck.addEventListener('click', async () => {
     
     // console.log(doc.getElementById('productTitle')) 
     // alert('this is working') 
-    let price;
-    fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
-        .then(response => response.text())
-        .then(data => {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(data, 'text/html')
+    // let price;
+    // fetch('https://www.amazon.com/Newest-HP-Pavilion-7-5700U-i7-1180G7/dp/B09QS7W8G5/ref=sr_1_19?crid=QJS9MMS6S6Y7&keywords=laptop&qid=1644909952&sprefix=laptop%2Caps%2C89&sr=8-19')
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         let parser = new DOMParser();
+    //         let doc = parser.parseFromString(data, 'text/html')
 
-            alert("this is at this part of the script")
-            console.log(doc)
+    //         alert("this is at this part of the script")
+    //         console.log(doc)
 
-            price = doc.getElementsByClassName('a-price-whole')[0].innerText;
-            console.log("product price ", price)
-        })
+    //         price = doc.getElementsByClassName('a-price-whole')[0].innerText;
+    //         console.log("product price ", price)
+    //     })
     
-    console.log(price)
+    // console.log(price)
+
+    chrome.runtime.sendMessage({greeting: "checkPrice"}, (response) => {
+        console.log(response.farewell)
+        alert(response.farewell)
+    })
 })
