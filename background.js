7fe406347781e3e8bea36231dc0950c33d743a97
Elyse Kriegel <esamkriegel@gmail.com>
2022-03-17 01:36:19 -0500

    added login functionality

--- background.js
+++ background.js
@@ -1,30 +1,38 @@
 /*
-*This is the 'main' javascript class 
-*It will run the alarms, storage, and price check scripts 
-*/
+ *This is the 'main' javascript class
+ *It will run the alarms, storage, and price check scripts
+ */
 
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason == 'install') {
-        chrome.tabs.create({ url: chrome.runtime.getURL("pages/installed.html")}); 
-    }
-    else if (details.reason = 'update') {
-        chrome.tabs.create({ url: chrome.runtime.getURL('pages/update.html')})
-    }
-})
+  if (details.reason == "install") {
+    chrome.tabs.create({ url: chrome.runtime.getURL("pages/installed.html") });
+  } else if ((details.reason = "update")) {
+    chrome.tabs.create({ url: chrome.runtime.getURL("pages/update.html") });
+  }
+});
 
 chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
-    console.log(sender.tab ? "from content script " + sender.tab.url : "from the extension")
+  console.log(
+    sender.tab ? "from content script " + sender.tab.url : "from the extension"
+  );
 
-    if(response.greeting === "checkPrice")
-    {
-        checkPrice()
-        sendResponse({farewell: "goodbye"})
+  if (response.greeting === "checkPrice") {
+    checkPrice();
+    sendResponse({ farewell: "goodbye" });
+  }
+});
+
+chrome.runtime.onMessageExternal.addListener(
+  (request, sender, sendResponse) => {
+    console.log(
+      sender.tab
+        ? "from content script " + sender.tab.url
+        : "from the extension"
+    );
+
+    if (request.test) {
+      sendResponse({ success: true, token: request.token });
+      localStorage.setItem("token", request.token); 
     }
-})
-
-
-// function onUpdate() {
-//     chrome.storage.sync.set({
-//         automat
-//     })
-// }
+  }
+);
